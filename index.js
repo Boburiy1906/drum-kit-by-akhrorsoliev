5a57be5d8af9010cbb9d1fced0b6e38d5d284950
Boburiy1906 <Boburshoh1906@gmail.com>
2023-02-01 15:34:21 +0500

    added elements, styles and script codes

---
+++ index.js
@@ -0,0 +1,46 @@
+const drums = document.querySelectorAll('.drum');
+drums.forEach((even) => {
+  even.addEventListener('click', (btn) => {
+    const text = even.textContent;
+    playMusic(text)
+  })
+  document.addEventListener('keydown', (key) => {
+    const keyText = key.key;
+    playMusic(keyText);
+  })
+});
+
+function playMusic(text) {
+  switch (text) {
+    case 'w':
+      const crash = new Audio("/sounds/crash.mp3")
+      crash.play();
+      break;
+    case 'a':
+      const kick = new Audio("/sounds/kick-bass.mp3")
+      kick.play();
+      break;
+    case 's':
+      const snare = new Audio('./sounds/snare.mp3');
+      snare.play();
+      break;
+    case 'd':
+      const tom1 = new Audio('/sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case 'j':
+      const tom2 = new Audio('/sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case 'k':
+      const tom3 = new Audio('/sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case 'l':
+      const tom4 = new Audio('/sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    default:
+      break;
+  }
+}
